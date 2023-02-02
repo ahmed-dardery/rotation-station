0488852ad2df593b83dae509bab49e46ef0ebe0a
Ahmed Dardery <megadardery@yahoo.com>
2023-02-02 00:10:58 +0000

    cleanup

---
+++ src/openings.ts
@@ -0,0 +1,13 @@
+export enum OPENINGS {
+    NONE = 0,
+    BOTTOM = 1,
+    RIGHT = 2,
+    TOP = 4,
+    LEFT = 8,
+}
+export function rotateClockwise(openings: OPENINGS): OPENINGS {
+    return ((openings << 3) | (openings >> 1)) & 0b1111;
+}
+export function rotateAntiClockwise(openings: OPENINGS): OPENINGS {
+    return ((openings >> 3) | (openings << 1)) & 0b1111;
+}
---
+++ src/svgs.ts
@@ -0,0 +1,38 @@
+import {OPENINGS} from "./openings";
+
+const filePath = (cell) => `assets/cells/${cell}.svg`
+
+export interface PieceInfo {
+    fileName: string;
+    initialOpenings: OPENINGS;
+}
+
+export const bar = {
+    fileName: filePath('bar'),
+    initialOpenings: OPENINGS.TOP | OPENINGS.BOTTOM,
+};
+
+export const corner = {
+    fileName: filePath('corner'),
+    initialOpenings: OPENINGS.TOP | OPENINGS.RIGHT,
+};
+
+export const full = {
+    fileName: filePath('full'),
+    initialOpenings: OPENINGS.NONE,
+};
+
+export const lone = {
+    fileName: filePath('lone'),
+    initialOpenings: OPENINGS.TOP,
+};
+
+export const shovel = {
+    fileName: filePath('shovel'),
+    initialOpenings: OPENINGS.TOP | OPENINGS.RIGHT | OPENINGS.BOTTOM,
+};
+
+export const shuriken = {
+    fileName: filePath('shuriken'),
+    initialOpenings: OPENINGS.TOP | OPENINGS.RIGHT | OPENINGS.BOTTOM | OPENINGS.LEFT,
+};
