0e1696dcc2611454c0428e1e77549933f8f4a86f
Ahmed Dardery <megadardery@yahoo.com>
2023-02-05 23:47:18 +0000

    improve deployment script

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
         main: "./src/app.ts",
     },
     output: {
-        path: path.resolve(__dirname, './dist'),
+        path: path.resolve(__dirname),
         filename: "app-bundle.js"
     },
     resolve: {
