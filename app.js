197afbc9bceb01e392e5e1c63dc70d50d84d5771
Ahmed Dardery <megadardery@yahoo.com>
2023-02-05 23:30:18 +0000

    Updates

--- app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var game_area_1 = require("./game_area");
-var levels_1 = require("./levels");
-var gameArea = game_area_1.GameArea.getInstance();
-gameArea.initialize((0, levels_1.constructLevel)(levels_1.LEVELS[1]));
-//# sourceMappingURL=app.js.map
