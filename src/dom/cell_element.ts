ee261521cf887cc98bb351bd8aa332861684a590
Ahmed Dardery <megadardery@yahoo.com>
2023-02-16 22:54:53 +0000

    add movement sound

--- src/dom/cell_element.ts
+++ src/dom/cell_element.ts
@@ -7,9 +7,15 @@ export class CellElement {
   private rotation: number;
   public openings: Openings;
 
+  moveSound = new Audio('assets/sounds/move.m4a');
+  failSound = new Audio('assets/sounds/fail.wav');
+
   constructor(openings: Openings, scaling: number, offset: number, i: number, j: number) {
     this.el = document.createElement('img');
     this.el.setAttribute('draggable', 'false');
+
+    this.moveSound.volume = 0.1;
+    this.failSound.volume = 0.02;
 
     this.updateOpenings(openings);
 
@@ -33,20 +39,24 @@ export class CellElement {
     animateRotate(this.el, this.rotation, this.rotation + 90);
     this.rotation = (this.rotation + 90) % 360;
     this.openings = rotateOpeningsClockwise(this.openings);
+    this.moveSound.play();
   }
 
   rotateAntiClockwise() {
     animateRotate(this.el, this.rotation, this.rotation - 90);
     this.rotation = (this.rotation + 360 - 90) % 360;
     this.openings = rotateOpeningsAntiClockwise(this.openings);
+    this.moveSound.play();
   }
 
   failRotateClockwise() {
     animateFailure(this.el, this.rotation, true);
+    this.failSound.play();
   }
 
   failRotateAntiClockwise() {
     animateFailure(this.el, this.rotation, false);
+    this.failSound.play();
   }
 
   adjust(edge: number, offset: number, i: number, j: number) {
